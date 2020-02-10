01b6a34994e880240b8c73e2152a4245b70fd680
Suman Barick <codotronix@gmail.com>
2020-02-11 00:45:45 +0530

    latest codebase

---
+++ precache-manifest.a5142d9494a12b73e1b725da505823eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629fcb212959a4c94022ed22fa0d9d0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a9eb2ae2e6cad826143",
+    "url": "/static/css/main.372481a6.chunk.css"
+  },
+  {
+    "revision": "1b72967c2dc8c33be2c8",
+    "url": "/static/js/2.babd82ea.chunk.js"
+  },
+  {
+    "revision": "4544b2b45c9cfea0feb3c391ccb163d8",
+    "url": "/static/js/2.babd82ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a9eb2ae2e6cad826143",
+    "url": "/static/js/main.10baa810.chunk.js"
+  },
+  {
+    "revision": "55536519e6cc577fa786",
+    "url": "/static/js/runtime-main.67da8b38.js"
+  }
+]);
